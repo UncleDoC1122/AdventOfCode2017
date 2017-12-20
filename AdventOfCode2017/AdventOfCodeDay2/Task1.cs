dc48fc1cd0cbb1ddbd0dc12c0cfb0467ab662413
Daniil Siyanchev <daniil.siyanchev@simbirsoft.com>
2017-12-20 16:17:03 +0400

    Added Day 2

---
+++ AdventOfCode2017/AdventOfCodeDay2/Task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay2
+{
+    class Task1
+    {
+        public static void Solve(string pathToFile)
+        {
+            var input = File.ReadAllLines(pathToFile);
+
+            var sum = 0;
+
+            foreach(var line in input)
+            {
+                var max = Int32.MinValue;
+                var min = Int32.MaxValue;
+
+                var numsArray = line.Split('\t');
+
+                foreach(var num in numsArray)
+                {
+                    if (int.Parse(num) > max)
+                        max = int.Parse(num);
+                    if (int.Parse(num) < min)
+                        min = int.Parse(num);
+                }
+
+                sum += max - min;
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ AdventOfCode2017/AdventOfCodeDay2/Task2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay2
+{
+    class Task2
+    {
+        public static void Solve(string pathToFile)
+        {
+            var input = File.ReadAllLines(pathToFile);
+
+            var sum = 0;
+
+            foreach (var line in input)
+            {
+                var max = Int32.MinValue;
+                var min = Int32.MaxValue;
+
+                var numsArray = line.Split('\t');
+
+                foreach (var num in numsArray)
+                {
+                    for (int i = 0; i < numsArray.Length; i ++)
+                    {
+                        if (int.Parse(num) % int.Parse(numsArray[i]) == 0 && int.Parse(num) != int.Parse(numsArray[i]))
+                            sum += int.Parse(num) / int.Parse(numsArray[i]);
+
+                    }
+                    
+                }
+
+                
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
