1af08df36ab11ac75a2953974a601bd9fbffef20
Daniil Siyanchev <daniil.siyanchev@simbirsoft.com>
2017-12-21 17:46:46 +0400

    Added new projects and deleted unused

--- AdventOfCode2017/AdventOfCodeDay3/Task1.cs
+++ AdventOfCode2017/AdventOfCodeDay3/Task1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace AdventOfCodeDay3
 {
     class Task1
     {
+        public static void Solve(string pathToFile)
+        {
+            var number = int.Parse(File.ReadAllText(pathToFile));
+
+            int iterator = 0;
+            int adder = 7;
+            int newNum = 1;
+            int depth = 0;
+
+            while(true)
+            {
+
+            }
+        }
+
     }
 }
---
+++ AdventOfCode2017/AdventOfCodeWeb2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdventOfCodeWeb2017.Startup))]
+namespace AdventOfCodeWeb2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
