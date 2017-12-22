015ebcff6ee03ae5962f4020fd59d33ca12ed632
Daniil Siyanchev <daniil.siyanchev@simbirsoft.com>
2017-12-22 16:32:32 +0400

    Added Day 9

---
+++ AdventOfCode2017/AdventOfCodeDay9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1and2.Solve(@"..\..\..\Input files\input - day 9.txt");
+        }
+    }
+}
---
+++ AdventOfCode2017/AdventOfCodeDay9/Task1and2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeDay9
+{
+    class Task1and2
+    {
+        public static void Solve(string pathToFile)
+        {
+            Stack<char> groups = new Stack<char>();
+            int score = 0;
+            int garbageCount = 0;
+            bool ignoring = false;
+
+            using (StreamReader sr = new StreamReader(pathToFile))
+            {
+
+                while (sr.Peek() >= 0)
+                {
+                    var character = (char)sr.Read();
+
+                    if (character.Equals('!'))
+                        sr.Read();
+                    else if (character.Equals('{') && !ignoring)
+                        groups.Push('{');
+                    else if (character.Equals('}') && !ignoring)
+                    {
+                        score += groups.Count;
+                        groups.Pop();
+                    }
+                    else if (character.Equals('<') && !ignoring)
+                        ignoring = true;
+                    else if (character.Equals('>') && ignoring)
+                        ignoring = false;
+                    else if (ignoring)
+                        garbageCount++;
+                }
+            }
+
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Garbage count: " + garbageCount);
+            Console.ReadKey();
+        }
+    }
+}
